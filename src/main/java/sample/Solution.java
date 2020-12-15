8ae2abaddbd8f657d7bde16afefebfce5b77f643
ACDYJ <2457709810@qq.com>
2020-12-16 00:00:13 +0800

    add new

--- src/main/java/sample/Solution.java
+++ src/main/java/sample/Solution.java
@@ -3,8 +3,13 @@ package sample;
 public class Solution {
     public static void main(String[] args) {
 
-        int a=1;
-        int b=2;
-        System.out.println(b/a);
+        try {
+            int a = 1;
+            int b = 2;
+            System.out.println(b / a);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }

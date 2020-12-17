8a59fae13b5ec43be09566dc44c6e0b8a361730f
ACDYJ <2457709810@qq.com>
2020-12-17 22:49:17 +0800

    feature y

--- src/main/java/sample/Solution2.java
+++ src/main/java/sample/Solution2.java
@@ -4,5 +4,9 @@ public class Solution2 {
     public static void main(String[] args) {
 
         System.out.println("yes this is solution2");
+        System.out.println("yes this is solution2");
+        System.out.println("yes this is solution2");
+        System.out.println("yes this is solution2");
+
     }
 }

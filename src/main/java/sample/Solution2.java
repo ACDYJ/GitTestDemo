9f88fda25a654007a6a86f195160f488b348c1dd
ACDYJ <2457709810@qq.com>
2020-12-17 23:04:09 +0800

    xiugai solution2

--- src/main/java/sample/Solution2.java
+++ src/main/java/sample/Solution2.java
@@ -3,10 +3,10 @@ package sample;
 public class Solution2 {
     public static void main(String[] args) {
 
-        System.out.println("yes this is solution2");
-        System.out.println("yes this is solution2");
-        System.out.println("yes this is solution2");
-        System.out.println("yes this is solution2");
+        System.out.println("yes this is solution21");
+        System.out.println("yes this is solution21");
+        System.out.println("yes this is solution21");
+        System.out.println("yes this is solution21");
 
     }
 }

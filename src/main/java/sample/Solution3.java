64cd2c58a2444bcf6d35ddd17da2a0315aa9bac1
ACDYJ <2457709810@qq.com>
2020-12-17 23:36:00 +0800

    join the new solutuion

--- src/main/java/sample/Solution3.java
+++ src/main/java/sample/Solution3.java
@@ -3,5 +3,8 @@ package sample;
 public class Solution3 {
     public static void main(String[] args) {
         System.out.println("This is Solution3");
+        System.out.println("This is Solution3");
+        System.out.println("This is Solution3");
+        System.out.println("This is Solution3");
     }
 }

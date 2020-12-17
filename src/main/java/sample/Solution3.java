df98a691c33d296f258cfdf441846bfe266274cb
ACDYJ <2457709810@qq.com>
2020-12-17 22:30:09 +0800

    add some in solution3

--- src/main/java/sample/Solution3.java
+++ src/main/java/sample/Solution3.java
@@ -3,5 +3,6 @@ package sample;
 public class Solution3 {
     public static void main(String[] args) {
         System.out.println("This is Solution3");
+        System.out.println("add something new");
     }
 }
--- src/main/java/sample/Solution4.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class Solution4 {
-    public static void main(String[] args) {
-        System.out.println("tube");
-    }
-}

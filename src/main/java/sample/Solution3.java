584346cfbfc473ab1c3dbb452c937d80d92b3047
ACDYJ <2457709810@qq.com>
2020-12-17 22:40:18 +0800

    fix bug

--- src/main/java/sample/Solution3.java
+++ src/main/java/sample/Solution3.java
@@ -3,6 +3,6 @@ package sample;
 public class Solution3 {
     public static void main(String[] args) {
         System.out.println("This is Solution3");
-        System.out.println("add something new");
+        System.out.println("add new  something");
     }
 }

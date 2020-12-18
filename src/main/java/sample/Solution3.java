69f4520c09c07937d100ca8ab589920e6631c3a5
ACDYJ <2457709810@qq.com>
2020-12-18 17:32:42 +0800

    提交分割代码

--- src/main/java/sample/Solution3.java
+++ src/main/java/sample/Solution3.java
@@ -7,5 +7,11 @@ public class Solution3 {
         System.out.println("This is Solution3");
         System.out.println("This is Solution3");
         System.out.println("This is Solution3");
+
+        String usertask="ding_11";
+        int taskId=Integer.valueOf(usertask.split("_")[1]);
+        String username=usertask.split("_")[0];
+        System.out.println(taskId);
+        System.out.println(username);
     }
 }
